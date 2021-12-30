1bc96ed59765d0c583343be4a6e147b074698420
Barrie McGuire <bmcguire@equalexperts.com>
2021-12-30 23:05:20 +0000

    day 1 part 1 solution

--- day1/src/advent/core.clj
+++ day1/src/advent/core.clj
@@ -8,13 +8,29 @@
     (clojure.string/split-lines 
       (slurp file)))
 
-(defn- get-int-array [lines]
+(defn- get-int-list [lines]
   (map #(Integer/parseInt %) lines))
+
+(defn- number-of-increases [depths]
+  (reduce 
+    (fn [state d]
+      (let 
+        [previous (get state :previous Integer/MAX_VALUE)
+         count    (get state :increase-count 0)]
+        (if (> d previous) 
+          {:previous d :increase-count (inc count)}
+          {:previous d :increase-count count})))
+    '{} 
+    depths))
+
+(defn- printy [{:keys [increase-count]}]
+  (println (string/join " " ["The number of times the depth increases is" increase-count])))
 
 (defn -main
   "Find the number of times the depth increases."
   []
   (-> data-file
     (read-lines)
-    (get-int-array)
-    (println)))
+    (get-int-list)
+    (number-of-increases)
+    (printy)))
